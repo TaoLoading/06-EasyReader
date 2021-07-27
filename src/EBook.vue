9f714b659eee850d2e8d3eb49a67a081574d3c6c
TaoLoading <1377226380@qq.com>
2021-07-27 16:45:40 +0800

    渲染电子书

---
+++ src/EBook.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read"></div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+import Epub from 'epubjs'
+
+const DOWNLOAD_URL = '/static/test01.epub'
+global.ePub = Epub
+export default {
+  methods: {
+    // 电子书解析和渲染
+    showEpub() {
+      // 1.生成Ebook
+      this.book = new Epub(DOWNLOAD_URL)
+      // 2.生成Rendition,通过Book.renderTo方法生成
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 3.通过Rendition.display渲染电子书
+      this.rendition.display()
+    }
+  },
+  mounted() {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import "assets/styles/global.scss";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
 import '../src/assets/styles/iconfont/iconfont.css'
 import '../src/assets/styles/global.scss'
 
@@ -7,4 +8,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')

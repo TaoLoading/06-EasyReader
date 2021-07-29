c3ce622858a8d50bc038a59d52cc17d3199a7de9
TaoLoading <1377226380@qq.com>
2021-07-29 14:27:52 +0800

    实现字号设置

--- src/EBook.vue
+++ src/EBook.vue
@@ -16,6 +16,9 @@
     </div>
     <!-- 底部菜单区 -->
     <bottom-bar :ifShow="ifShow"
+                :fontSizeList="fontSizeList"
+                :defaultFontSize="defaultFontSize"
+                @setFontSize="setFontSize"
                 ref="bottomBar"></bottom-bar>
   </div>
 </template>
@@ -34,12 +37,24 @@ export default {
   },
   data() {
     return {
-      ifShow: false
+      ifShow: false,
+      // 字号列表
+      fontSizeList: [
+        { fontSize: 12 },
+        { fontSize: 14 },
+        { fontSize: 16 },
+        { fontSize: 18 },
+        { fontSize: 20 },
+        { fontSize: 22 },
+        { fontSize: 24 },
+      ],
+      defaultFontSize: 16
     }
   },
   methods: {
-    // 电子书解析和渲染
+    // 解析电子书
     showEpub() {
+      // 电子书的渲染
       // 1.生成Ebook
       this.book = new Epub(DOWNLOAD_URL)
       // 2.生成Rendition,通过Book.renderTo方法生成
@@ -49,6 +64,12 @@ export default {
       })
       // 3.通过Rendition.display渲染电子书
       this.rendition.display()
+
+      // 修改字体
+      // 获取Theme对象
+      this.themes = this.rendition.themes
+      // 设置默认字体
+      this.setFontSize(this.defaultFontSize)
     },
     // 返回上一页
     prevPage() {
@@ -62,11 +83,19 @@ export default {
         this.rendition.next()
       }
     },
+    // 菜单区的展示与隐藏
     show() {
       this.ifShow = !this.ifShow
       if (this.ifShow == false) {
         // 隐藏字体设置区
         this.$refs.bottomBar.hideSetting()
+      }
+    },
+    // 设置字体
+    setFontSize(fontSize) {
+      this.defaultFontSize = fontSize
+      if (this.themes) {
+        this.themes.fontSize(fontSize + 'px')
       }
     }
   },
--- src/components/bottomBar.vue
+++ src/components/bottomBar.vue
@@ -3,7 +3,8 @@
     <transition name="slideUp">
       <!-- 底部菜单区 -->
       <div class="bottomWrapper"
-           v-show="ifShow">
+           v-show="ifShow"
+           :class="{'hideBoxShadow':ifSettingShow || !ifShow}">
         <div class="iconWrapper">
           <span class="iconfont  icon-liebiao icon"></span>
         </div>
@@ -24,7 +25,26 @@
       <div class="settingWrapper"
            v-show="ifSettingShow">
         <!-- 字体设置区 -->
-        <div class="fontSize"></div>
+        <div class="fontSize">
+          <div class="preview"
+               :style="{fontSize: this.fontSizeList[0].fontSize + 'px'}">A</div>
+          <!-- 字体调节区 -->
+          <div class="selectFontSize"
+               v-for="(item, index) in fontSizeList"
+               :key="index"
+               @click="setFontSize(item.fontSize)">
+            <div class="line"></div>
+            <div class="pointWrapper">
+              <div class="point"
+                   v-show="defaultFontSize === item.fontSize">
+                <div class="smallPoint"></div>
+              </div>
+            </div>
+            <div class="line"></div>
+          </div>
+          <div class="preview"
+               :style="{fontSize: this.fontSizeList[fontSizeList.length - 1].fontSize + 'px'}">A</div>
+        </div>
       </div>
     </transition>
   </div>
@@ -37,19 +57,28 @@ export default {
     ifShow: {
       type: Boolean,
       default: false
-    }
+    },
+    fontSizeList: Array,
+    defaultFontSize: Number
   },
   data() {
     return {
-      ifSettingShow: false
+      ifSettingShow: false,
     }
   },
   methods: {
+    // 展示设置区
     showSetting() {
       this.ifSettingShow = !this.ifSettingShow
     },
+    // 隐藏设置区
     hideSetting() {
       this.ifSettingShow = false
+    },
+    // 设置字体
+    setFontSize(fontSize) {
+      // 调用父组件的setFontSize()，因为调节字体需要用到epubjs
+      this.$emit('setFontSize', fontSize)
     }
   }
 }
@@ -62,12 +91,15 @@ export default {
     position: absolute;
     bottom: 0;
     left: 0;
-    z-index: 101;
+    z-index: 102;
     display: flex;
     width: 100%;
     height: px2rem(48);
     background: #fff;
     box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+    &.hideBoxShadow {
+      box-shadow: none;
+    }
     .iconWrapper {
       flex: 1;
       @include center;
@@ -78,13 +110,55 @@ export default {
   }
   .settingWrapper {
     position: absolute;
-    bottom: px2rem(48);
+    bottom: px2rem(47);
     left: 0;
     width: 100%;
     height: px2rem(48);
     background: #fff;
+    z-index: 101;
     box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
     .fontSize {
+      display: flex;
+      height: 100%;
+      .preview {
+        flex: 0 0 px2rem(40);
+        @include center;
+      }
+      .selectFontSize {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        .line {
+          flex: 1;
+          height: 0;
+          border-top: px2rem(1) solid #ccc;
+        }
+        .pointWrapper {
+          position: relative;
+          flex: 0 0 0;
+          width: 0;
+          height: px2rem(7);
+          border-left: px2rem(1) solid rgb(133, 133, 133);
+          .point {
+            position: absolute;
+            top: px2rem(-7);
+            left: px2rem(-10);
+            width: px2rem(20);
+            height: px2rem(20);
+            border-radius: 50%;
+            background: #fff;
+            border: px2rem(1) solid #ccc;
+            box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, 0.15);
+            @include center;
+            .smallPoint {
+              width: px2rem(5);
+              height: px2rem(5);
+              background: #000;
+              border-radius: 50%;
+            }
+          }
+        }
+      }
     }
   }
 }
